00d8c1e57b66664debadfd734b78f474bb76361e
ug-kim <ugkim816@gmail.com>
2020-02-12 02:36:28 +0900

    remove node_modules

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,100 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from 'styled-components';
 import Header from '../components/Header';
 
-const LoginPage = props => {
+const BlockHeader = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const TitleHeader = styled.div`
+  text-align: center;
+`;
+
+const TextHeader = styled.span`
+  font-weight: bold;
+  font-size: 25px;
+  line-height: 60px;
+  vertical-algin: middle;
+`;
+
+const ImageHeader = styled.img`
+  width: 60px;
+  height: 60px;
+  vertical-align: middle;
+`;
+
+const LoginHeader = styled.input`
+  width: 300px;
+  height: 30px;
+  font-size: 15px;
+  margin-top: 10px;
+  outline: 1px solid black;
+`
+
+const ButtonHeader = styled.button`
+  width: 304px;
+  height: 50px;
+  margin-top: 20px;
+  border: none;
+  background-color: #4CAF50;
+  color: white;
+  text-aling: center;
+  display: inline-block;
+  font-size: 16px;
+`
+
+function LoginPage() {
+  const [inputs, setInputs] = useState({
+    id: '',
+    password: ''
+  });
+
+  const {id, password} = inputs;
+
+  const onChange = (e) => {
+    const{value, name} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    })
+  }
+
+  const onClick = () => {
+    setInputs({
+      id: '',
+      password: ''
+    })
+  }
+
   return (
     <>
-      <Header />
-      <div>LoginPage!!</div>
+      <BlockHeader>
+        <TitleHeader>
+          <ImageHeader src={require("../images/mamago_logo.png")} />
+          <TextHeader>Mamago</TextHeader>
+        </TitleHeader>
+        <div>
+          <LoginHeader name="id" placeholder="아이디" onChange={onChange} value={id} />
+        </div>
+        <div>
+          <LoginHeader name="password" placeholder="비밀번호" onChange={onChange} value={password}/>
+        </div>
+        <div>
+          <ButtonHeader onClick={onClick}>로그인</ButtonHeader>
+        </div>
+      </BlockHeader>
+
+      <div>
+        <b>value: </b>
+          {id} ({password})
+      </div>
     </>
-  );
-};
+  )
+
+}
+
 
 export default LoginPage;
