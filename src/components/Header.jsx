a69300ec83a949aba551c5e2afd6654f9c060a1d
unknown <kaist984@example.com>
2020-02-12 02:10:17 +0900

    add header icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,13 +10,43 @@ const StyledHeader = styled.div`
   background-color: ${props => (props.danger ? 'red' : 'purple')};
 `;
 
+const FontsHeader = styled.span`
+  width: 40px;
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+  vertical-align: middle;
+  margin-top: auto;
+  margin-bottom: auto;
+  align-content: center;
+  @font-face {
+    font-family: "08SeoulNamsanM";
+    font-weight: 600;
+    src: url("../fonts/08SeoulNamsanM.ttf") format("truetype");
+  }
+`
+
+const Logo = styled.img`
+  width 40px;
+  height 40px;
+  vertical-align: middle;
+`;
+
 const Header = props => {
   const { children } = props;
   return (
-    <StyledHeader>
-      Header
-      {children}
-    </StyledHeader>
+      <StyledHeader>
+        <nav> 
+          <span>
+            <Logo src={require("../images/main_icon.png")}></Logo>
+            <FontsHeader>Mamago</FontsHeader>
+          </span>
+          <span>
+            <button onClick src={require("../icons/menu.png")}></button>
+          </span>
+        </nav>
+        {children}
+      </StyledHeader>
   );
 };
 
