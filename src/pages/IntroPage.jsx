67f380429ad43588e1a35ff86009e54616fac5a0
Soulmates2 <kaist984@kaist.ac.kr>
2020-02-13 20:40:19 +0900

    Intro Page

--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -1,15 +1,72 @@
 import React from 'react';
+import styled from 'styled-components';
 import Header from '../components/Header';
 import Main from '../components/Main';
+import '../font.css';
+
+const Text = styled.div`
+  max-width: 100%;
+  left: 26px;
+  top: 66px;
+  font-size: 2rem;
+  padding: 2px;
+  font-weight: bold;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`
+
+const Image = styled.img`
+  border: none;
+  width: 100%;
+  height: 60%;
+  text-align: center;
+  vertical-align: middle;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 3px;
+`
+
+const Talk = styled.div`
+  position: fixed;
+  width: 50%;
+  height: 3rem;
+  line-height: 3rem;
+  text-align: center;
+  font-size: 1rem;
+  color: white;
+  font-weight: bold;
+  bottom: 0;
+  left: 0;
+  background-color: #3DC728;
+`
+
+const PastLog = styled.div`
+  position: fixed;
+  width: 50%;
+  height: 3rem;
+  line-height: 3rem;
+  font-size: 1rem;
+  text-align: center;
+  color: white;
+  font-weight: bold;
+  bottom: 0;
+  right: 0;
+  background-color: #5D90F5;
+`
+
 
 const IntroPage = props => {
   return (
     <>
       <Header />
       <Main>
-        <div>인공지능과 어디서나 언제든지!</div>
-        <div>하루 10분으로 영어 실력을 키워요!</div>
+        <Text>인공지능 영어튜터와</Text>
+        <Text>어디서나 언제든지!</Text>
+        <Text>하루 10분으로</Text>
+        <Text>영어 실력을 키워요!</Text>
+        <Image src={require('../images/mamago_logo.png')}></Image>
       </Main>
+      <Talk>대화하기</Talk>
+      <PastLog>지난 기록</PastLog>
     </>
   );
 };
