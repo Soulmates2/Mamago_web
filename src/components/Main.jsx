5f0c5536bd554defc854de394ac60c4f5d8b4400
Kunho Kim <46233964+Soulmates2@users.noreply.github.com>
2020-02-13 13:53:24 +0900

    make header component
    Co-authored-by: Kim Yuji <38632805+ug-kim@users.noreply.github.com>

---
+++ src/components/Main.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
+const MainStyle = styled.div`
+  padding-top: 50px;
+  position: absolute;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+`;
+
+
+
+const Main = props => {
+  const { children } = props;
+
+  return (
+      
+      <MainStyle>
+          {children}
+      </MainStyle>
+  );
+};
+
+export default Main;
