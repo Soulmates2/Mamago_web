6b07ba10ddedc4dd0c5243375c66859d9fb93812
Kunho Kim <46233964+Soulmates2@users.noreply.github.com>
2020-02-13 17:07:15 +0900

    Add Chatting Bubble (#5)
    * chatting page base
    
    * Apply basement every page
    
    * Add Chatting Bubble

--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -7,7 +7,8 @@ const IntroPage = props => {
     <>
       <Header />
       <Main>
-        <div>IntroPage!!</div>
+        <div>인공지능과 어디서나 언제든지!</div>
+        <div>하루 10분으로 영어 실력을 키워요!</div>
       </Main>
     </>
   );
