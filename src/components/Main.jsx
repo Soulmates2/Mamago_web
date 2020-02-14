98e2171da682a032a50c0435e3c68bced197da2c
Soulmates2 <kaist984@kaist.ac.kr>
2020-02-14 09:51:27 +0900

    Before onClick and API communication

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const MainStyle = styled.div`
-//   padding-top: 4rem;
-//   padding-left: 0.5rem;
+  padding-top: 4rem;
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
   position: absolute;
   z-index: 1;
   width: 100%;
